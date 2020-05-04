768be209bc9b69811570f28543cea8458e55a73a
valeria56467 <valeria56467@users.noreply.github.com>
2020-05-04 22:38:09 +0200

    Анимация

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Привет!")
+const cartButton=document.querySelector("#cart-button")
+console.log(cartButton)
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close")
+cartButton.addEventListener('click', function (event) {modal.classList.add("is-open")})
+
+close.addEventListener('click', function (event) {modal.classList.remove("is-open")})
+
+new WOW().init();
